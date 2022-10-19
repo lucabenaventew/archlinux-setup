7c22bb0b414b6193f2c257372d63d9f126fec557
lucabenaventew <cacachuuuluca@gmail.com>
2022-10-19 00:25:49 -0500

    Update

--- nvcustom/plugins.lua
+++ nvcustom/plugins.lua
@@ -6,12 +6,17 @@ return {
   ["Pocco81/AutoSave.nvim"] = {},
   ["github/copilot.vim"] = {},
   ["neoclide/coc.nvim"] = {
+    disable = true,
     branch = master,
     run = "yarn install --frozen-lockfile && yarn compile",
   },
 
   -- NVC Plugins
   ["goolord/alpha-nvim"] = {
-    disable = false
+    disable = false,
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = function ()
+      require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    end
   }
 }
